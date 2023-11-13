e9328d870af5158405cc6694efef72429cdf8d29
Joshua Jones <joshua.davidjones92@gmail.com>
2023-11-13 11:18:27 +0000

    Write function to add new book object to library array

--- script.js
+++ script.js
@@ -2,16 +2,29 @@
 const Library = [];
 
 // The book constructor
-function Book(author, title, pages){
-    this.author = author;
+function Book(title, author, pages, read){
     this.title = title;
+    this.author = author;
     this.pages = pages;
+    this.read = read;
 };
 
 // Function that adds book to library array
 function addBookToLibrary(){
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('read').checked;
 
+    let newBook = new Book(title, author, pages, read)
+    console.log(newBook);
+    Library.push(newBook);
 };
+
+document.getElementById('bookForm').addEventListener('submit', function(event){
+    event.preventDefault();
+    addBookToLibrary()
+});
 
 // Handles pop up form opening and closing
 document.addEventListener('DOMContentLoaded', function () {
@@ -19,11 +32,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const closeFormButton = document.getElementById('closeForm');
     const overlay = document.getElementById('overlay');
 
-    // Makes form appear
+    // Makes form visible
     newBook.addEventListener('click', function () {
         overlay.style.display = 'flex';
     });
-    // Makes form non visible
+    // Closes form
     closeFormButton.addEventListener('click', function () {
         overlay.style.display = 'none';
     });
